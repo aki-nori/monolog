acc350a74181c3b5785fdd527dae60a2c56c5fc7
aki-nori <i.akinori09@gmail.com>
2020-02-10 07:22:18 +0000

    [Add]アイテム削除機能

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
 
+  describe 'GET #show' do
+    user = create(:user)
+    get :show, id: user
+    expect(assigns(:article)).to eq 
+  end
+
 end
