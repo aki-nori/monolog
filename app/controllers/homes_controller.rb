eefc8cd99931105ea3ed1cf4f8256749a6eb6b8f
aki-nori <i.akinori09@gmail.com>
2020-02-21 05:55:13 +0000

    [Clean]管理画面リファクタリング（controller側での処理集約）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -18,9 +18,21 @@ class HomesController < ApplicationController
 
   def manage
     authenticate_admin!
-  	@users = User.all
-  	@items = Item.all
-  	@logs = Log.all
-  	@categories = Category.all
+
+    users = User.all
+  	@users_count = users.count
+    @recent_user_create = users.order(created_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
+    @recent_user_update = users.order(updated_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
+
+    categories = Category.all
+    @categories_count = categories.count
+    @recent_category_create = categories.order(created_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
+    @recent_category_update = categories.order(updated_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
+
+    items = Item.all
+    @items_count = items.count
+    @recent_item_create = items.order(created_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
+    @recent_item_update = items.order(updated_at: "DESC").first.created_at.strftime("%Y年%m月%d日 %H時%M分")
   end
+
 end
